beeb241174f62c3ba756b7d39961279e7fcd0286
Gaurav Chauriya <gaurav.chauriya@cuelogic.co.in>
2016-09-30 14:41:31 +0530

    friend api assignment

--- crud-using-hapi/config/database-connection.js
+++ crud-using-hapi/config/database-connection.js
@@ -2,32 +2,69 @@
 
 const configs      = require('./config.js');
 const mysql      = require('mysql');
+var connection = '';
+var isUserLoggedIn = false;
 
-
-var findData = function (sql, values, cb){
-  const connection = mysql.createConnection({
+var connectDb = function (authKey, cb){
+  connection = mysql.createConnection({
     host     : configs.database.host,
     user     : configs.database.username,
     password : configs.database.password,
     database : configs.database.db
   });
-
   connection.connect();
-
-  connection.query({
-      sql: sql,
-      timeout: 40000,
-      values: values,
-      }, function(err, rows, fields) {
-          if (err) throw err;
-          cb && cb(rows);
-    });
-
-  connection.end();
-
+  if(authKey && authKey !== ''){
+    connection.query({
+        sql: 'select id from users where login_auth_key = ?;',
+        timeout: 40000,
+        values: [authKey],
+        }, function(err, rows, fields) {
+            if (err) throw err;
+            if(rows.length > 0){
+              isUserLoggedIn = true;
+              cb && cb(true);
+            } else {
+              isUserLoggedIn = false;
+              connection.end();
+              cb && cb(false);
+            }
+      });
+  }
 }
 
+var endConnection = function () {
+  connection.end();
+}
+
+var findData = function (sql, values, cb){
+
+    connection.query({
+        sql: sql,
+        timeout: 40000,
+        values: values,
+        }, function(err, rows, fields) {
+            if (err) throw err;
+            cb && cb(rows);
+      });
+}
+
+var createFriends = function (userId, friends){
+  var friendList = friends.split(",");
+
+  connection.query({sql: "DELETE FROM `friends` WHERE `user_id` = ?", timeout: 40000, values: [userId] });
+
+  friendList.forEach(function (key, value){
+    console.log(key + '===' + value);
+    connection.query({sql: "INSERT INTO `friends`(`user_id`, `friend_id`) \
+                    VALUES (?, (select id from users where name = ? limit 1) )",
+                    timeout: 40000,
+                    values: [userId, key] });
+  });
+}
 
 module.exports = {
+  connectDb: connectDb,
   findData: findData,
+  endConnection: endConnection,
+  createFriends: createFriends
 }
--- crud-using-hapi/index.js
+++ crud-using-hapi/index.js
@@ -4,18 +4,51 @@ const Hapi = require('hapi');
 const configs = require('./config/config.js');
 const DB = require('./config/database-connection');
 const Joi = require('joi');
+const uuid = require('node-uuid');
 
 const server = new Hapi.Server();
 
 server.connection({host: configs.server.host, port: configs.server.port });
 
 server.route({
+    method: 'POST',
+    path: '/login',
+    handler: function (request, reply) {
+      DB.connectDb();
+      var data = DB.findData("select id from users where is_deleted = 0 and username = ? and password= ?",
+                      [request.payload.username,request.payload.password], function(data){
+            if(data.length > 0 && data[0].id){
+              var loginKey = uuid.v1();
+              DB.findData("UPDATE `users` SET `login_auth_key` = ? WHERE `id` = ?;",[loginKey, data[0].id],function(data){
+                DB.endConnection();
+                reply(loginKey);
+              });
+            }else{
+              DB.endConnection();
+              reply('No record Found');
+            }
+      });
+    }
+});
+
+server.route({
     method: 'GET',
     path: '/task',
     handler: function (request, reply) {
-      var data = DB.findData("select * from users where is_deleted = ?",["0"], function(data){
-          reply(data);
+      if(!request.headers.authorization){
+        reply('User Not Logged In.Auth')
+      }
+      DB.connectDb(request.headers.authorization, function (success){
+        if(!success){
+          reply('Un Authorised Access!');
+        }else{
+          var data = DB.findData("select * from users where is_deleted = ?",["0"], function(data){
+              DB.endConnection();
+              reply(data);
+          });
+        }
       });
+
     }
 });
 
@@ -23,10 +56,21 @@ server.route({
     method: 'GET',
     path: '/task/{id}',
     handler: function (request, reply) {
-      console.log(request.params.id);
-      var data = DB.findData("select * from users where is_deleted = 0 and id = ?",[request.params.id], function(data){
-          reply(data);
+      if(!request.headers.authorization){
+        reply('User Not Logged In.Auth')
+      }
+      DB.connectDb(request.headers.authorization, function (success){
+        if(!success){
+          reply('Un Authorised Access!');
+        }else{
+          var data = DB.findData("select * from users where is_deleted = 0 and id = ?",[request.params.id],
+            function(data){
+              DB.endConnection();
+              reply(data);
+          });
+        }
       });
+
     }
 });
 
@@ -34,8 +78,18 @@ server.route({
     method: 'DELETE',
     path: '/task/{id}',
     handler: function (request, reply) {
-      var data = DB.findData("Delete from users where id = ?",[request.params.id], function(data){
-          reply(data);
+      if(!request.headers.authorization){
+        reply('User Not Logged In.Auth')
+      }
+      DB.connectDb(request.headers.authorization, function (success){
+        if(!success){
+          reply('Un Authorised Access!');
+        }else{
+          var data = DB.findData("Delete from users where id = ?",[request.params.id], function(data){
+              DB.endConnection();
+              reply(data);
+          });
+        }
       });
     }
 });
@@ -44,20 +98,34 @@ server.route({
     method: 'POST',
     path: '/task',
     handler: function (request, reply) {
-      console.log(request.payload)
-      var data = DB.findData("INSERT INTO `my_test_db`.`users` (`name`, `email`, `phone`, `address`) VALUES ( ?, ?, ?, ?);",
-                            [request.payload.name,request.payload.email,request.payload.phone,request.payload.address],
-                 function(data){
-                      reply(data);
-                  });
+      if(!request.headers.authorization){
+        reply('User Not Logged In.Auth')
+      }
+      DB.connectDb(request.headers.authorization, function (success){
+        if(!success){
+          reply('Un Authorised Access!');
+        }else{
+          var data = DB.findData("INSERT INTO `my_test_db`.`users` (`name`, `username`, `password`, `email`, `phone`, `address`) VALUES ( ?, ?, ?, ?, ?, ?);",
+                                [request.payload.name,request.payload.username,request.payload.password,request.payload.email,request.payload.phone,request.payload.address],
+             function(data){
+                DB.createFriends(data.insertId,request.payload.friends)
+                DB.endConnection();
+                reply(data);
+              });
+        }
+      });
+
     },
     config: {
         validate: {
           payload: {
               name: Joi.string().required(),
+              username: Joi.string().required(),
+              password: Joi.string().required(),
               email: Joi.string().email(),
               phone: Joi.number(),
-              address: Joi.string().max(255)
+              address: Joi.string().max(255),
+              friends: Joi.string()
             }
         }
     }
@@ -67,24 +135,67 @@ server.route({
     method: 'PUT',
     path: '/task/{id}',
     handler: function (request, reply) {
-      console.log(request.params)
-      var data = DB.findData("UPDATE `users` SET `name` = ?, `email` = ?, `phone` = ?, `address` = ? WHERE `id` = ?;",
-                            [request.payload.name,request.payload.email,request.payload.phone,request.payload.address,request.params.id],
-                 function(data){
-                      reply(data);
+      if(!request.headers.authorization){
+        reply('User Not Logged In.Auth')
+      }
+      DB.connectDb(request.headers.authorization, function (success){
+        if(!success){
+          reply('Un Authorised Access!');
+        }else{
+          var data = DB.findData("UPDATE `users` SET `name` = ?, `username` = ?, `password` = ?, `email` = ?,\
+                  `phone` = ?, `address` = ? WHERE `id` = ?;",
+                  [request.payload.name, request.payload.username, request.payload.password,
+                  request.payload.email, request.payload.phone, request.payload.address, request.params.id],
+                  function(data){
+                    DB.createFriends(request.params.id, request.payload.friends)
+                    DB.endConnection();
+                    reply(data);
                   });
+        }
+      });
+
     },
     config: {
         validate: {
           payload: {
               name: Joi.string().required(),
+              username: Joi.string().required(),
+              password: Joi.string().required(),
               email: Joi.string().email(),
               phone: Joi.number(),
-              address: Joi.string().max(255)
+              address: Joi.string().max(255),
+              friends: Joi.string()
             }
         }
     }
 });
+
+server.route({
+    method: 'GET',
+    path: '/friend/{id}/{name?}',
+    handler: function (request, reply) {
+      if(!request.headers.authorization){
+        reply('User Not Logged In.Auth')
+      }
+      DB.connectDb(request.headers.authorization, function (success){
+        if(!success){
+          reply('Un Authorised Access!');
+        }else{
+          var name = request.params.name ? "users.name = '"+request.params.name+"'" : 'users.name IS Not Null';
+          console.log(name)
+          var data = DB.findData("SELECT users.name,users.email,users.phone,users.address \
+                      FROM `friends` inner join users on(users.id = friends.friend_id) \
+                      WHERE friends.user_id = ? and "+name,[request.params.id],
+            function(data){
+              DB.endConnection();
+              reply(data);
+          });
+        }
+      });
+
+    }
+});
+
 
 server.start(function(err){
 
